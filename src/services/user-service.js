01d67cb20580990ba879e9eb70a8b6a8377d4399
Omkar Harade <omkarharade1301@gmail.com>
2023-06-02 22:25:59 +0530

    added isAuthenticated function

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -2,6 +2,7 @@ const UserRepository = require("../repository/user-repository");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { JWT_KEY } = require("../config/serverConfig");
+const { use } = require("../routes");
 class UserService {
 	constructor() {
 		this.userRepository = new UserRepository();
@@ -69,6 +70,26 @@ class UserService {
 			return bcrypt.compareSync(userInputPlainPassword, encryptedPassword);
 		} catch (error) {
 			console.log("Something went wrong in the password comparison");
+			throw error;
+		}
+	}
+
+	async isAuthenticated(token) {
+		try {
+			const response = this.verifyToken(token);
+
+			if (!response) {
+				throw { error: "Invalid Token" };
+			}
+			const user = this.userRepository.getById(response.id);
+
+			if (!user) {
+				throw { error: "No user with the corresponding token exists" };
+			}
+
+			return user.id;
+		} catch (error) {
+			console.log("Something went wrong in the auth process");
 			throw error;
 		}
 	}
